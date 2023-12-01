0826e56d33611e9f5976d9e1eeeea914e1786fd5
mirsch <ms@splinelab.de>
2023-12-01 14:04:44 +0100

    day 1; file() instead of file_get_contents() and explode()

--- day01/1.php
+++ day01/1.php
@@ -1,10 +1,8 @@
 <?php
 
-$content = file_get_contents($argv[1]);
-$lines = explode("\n", $content);
+$lines = file($argv[1]);
 $sum = 0;
 foreach ($lines as $line) {
-    if ($line === '') continue;
     preg_match_all('/[\d]/', $line, $matches);
     $num = $matches[0][0] . $matches[0][count($matches[0]) - 1];
     $sum += (int) $num;
